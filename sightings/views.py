5367ff5ad3470880fde4decc0ae8f796fa6f4f44
Zhongyuan Gu <zg2351@columbia.edu>
2019-12-06 20:29:00 +0000

    views.py_v1

--- sightings/views.py
+++ sightings/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from adopt.models import show
+import random
 
-# Create your views here.
+def all_squirrels(request):
+    squirrels = list(show.objects.all())
+    context = {'squirrels': squirrels}
+    return render(request, 'adopt/list_all.html', context)
+
