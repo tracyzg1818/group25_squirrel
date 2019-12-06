495ff9f10db894baa612d1deebb04355cf660a59
Ruochen Zhong <rz2476@columbia.edu>
2019-12-06 21:17:28 +0000

    change a little error

--- group25project/urls.py
+++ group25project/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('', include('adopt.urls')),
+    path('', include('sightings.urls')),
     path('admin/', admin.site.urls),
 ]
